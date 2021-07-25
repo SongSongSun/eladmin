9e1d74125a5696842d36c200b7e7cd809e26b395
SongSongSun <49385128+songsongsun@users.noreply.github.com>
2021-07-25 20:31:54 +0800

    提交分支测试

--- eladmin-system/src/main/java/me/zhengjie/config/ConfigurerAdapter.java
+++ eladmin-system/src/main/java/me/zhengjie/config/ConfigurerAdapter.java
@@ -34,7 +34,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 public class ConfigurerAdapter implements WebMvcConfigurer {
 
-    /** 文件配置 */
+    /**
+     * 文件配置
+     */
     private final FileProperties properties;
 
     public ConfigurerAdapter(FileProperties properties) {
@@ -56,8 +58,8 @@ public class ConfigurerAdapter implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         FileProperties.ElPath path = properties.getPath();
-        String avatarUtl = "file:" + path.getAvatar().replace("\\","/");
-        String pathUtl = "file:" + path.getPath().replace("\\","/");
+        String avatarUtl = "file:" + path.getAvatar().replace("\\", "/");
+        String pathUtl = "file:" + path.getPath().replace("\\", "/");
         registry.addResourceHandler("/avatar/**").addResourceLocations(avatarUtl).setCachePeriod(0);
         registry.addResourceHandler("/file/**").addResourceLocations(pathUtl).setCachePeriod(0);
         registry.addResourceHandler("/**").addResourceLocations("classpath:/META-INF/resources/").setCachePeriod(0);
